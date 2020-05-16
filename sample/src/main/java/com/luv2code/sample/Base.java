5f77dc8dd2a6bf3ab4637ed087ae54443c535a8a
Ravi Shankar Jha <ravishankarjha163@gmail.com>
2020-05-16 21:55:21 +0530

    modifying base.java

--- sample/src/main/java/com/luv2code/sample/Base.java
+++ sample/src/main/java/com/luv2code/sample/Base.java
@@ -2,7 +2,6 @@ package com.luv2code.sample;
 
 public class Base {
 	public static void main(String [] args) {
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!!how are you doing people");
 	}
-	
 }
